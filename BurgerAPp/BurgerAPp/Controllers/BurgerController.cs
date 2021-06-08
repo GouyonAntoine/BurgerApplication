1bdf01bed7bc4fa865120ae82d6137555d7dae9f
GouyonAntoine <82935541+GouyonAntoine@users.noreply.github.com>
2021-06-08 15:45:41 +0200

    Ajout BeverageController

--- BurgerAPp/BurgerAPp/Controllers/BurgerController.cs
+++ BurgerAPp/BurgerAPp/Controllers/BurgerController.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebFastFood.Controllers
+namespace BurgerApp.Controllers
 {
     public class BurgersController : Controller
     {
