9a726e2a837c7f8911749627b0049d0cca483a1a
GouyonAntoine <82935541+GouyonAntoine@users.noreply.github.com>
2021-06-08 14:02:37 +0200

    Ajout Model Side/Product

---
+++ BurgerAPp/Side.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Side : Product
+    {
+        [Column("SideWeight")]
+        public int Weight { get; set; }
+        public int SaltWeight { get; set; }
+    }
+}
