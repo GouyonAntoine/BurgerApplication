cbcfbb17eea435e81a5fb14adbe9458d6ea9ab77
GouyonAntoine <82935541+GouyonAntoine@users.noreply.github.com>
2021-06-08 14:43:12 +0200

    Ajout Burger/IBurger Repository

---
+++ BurgerAPp/BurgerAPp/Repository/BurgerRepository.cs
@@ -0,0 +1,51 @@
+﻿using BurgerAPp.Repository.IRepositories;
+using Dal;
+using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BurgerAPp.Repository
+{
+    public class BurgerRepository : IBurgerRepository
+    {
+        private RestoContext context;
+        public BurgerRepository(RestoContext context)
+        {
+            this.context = context;
+        }
+
+        public void AddBurger(Burger burger)
+        {
+            context.Burgers.Add(burger);
+            context.SaveChanges();
+        }
+
+        public void DeleteBurger(Burger burger)
+        {
+            context.Burgers.Remove(burger);
+            context.SaveChanges();
+        }
+
+        public IQueryable<Burger> GetBurgers()
+        {
+            return context.Burgers;
+        }
+
+        public Burger GetBurger(int id)
+        {
+            return context.Burgers.FirstOrDefault(b => b.Id == id);
+        }
+
+        public void EditBurger(int id, Burger burger)
+        {
+            var burgerBDD = GetBurger(id);
+            burgerBDD.BeefWeight = burger.BeefWeight;
+            burgerBDD.Description = burger.Description;
+            burgerBDD.Name = burger.Name;
+            burgerBDD.Price = burger.Price;
+            burgerBDD.Weight = burger.Weight;
+        }
+    }
+}
